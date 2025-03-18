1301a94bae9e24d8db51fe68b93224907da10a38
Andres Ignacio Vargas Villanueva <Andres@MacBook-Air-de-Andres-Vargas.local>
2025-03-17 22:24:48 -0500

    update

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-4871c466-b559-482c-b607-ee5f3d554862.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-8ff5f5fc-e802-424d-b08e-ed3fea8d2597.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
