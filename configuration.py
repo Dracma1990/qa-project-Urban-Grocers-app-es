fb2a04ef4c0617f68eb42823188e525151b7be08
Andres <vargasandres.md@gmail.com>
2025-03-17 22:54:33 -0500

    update

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-8ff5f5fc-e802-424d-b08e-ed3fea8d2597.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-cd7f8273-7972-495e-a9bd-39b782432d8e.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
